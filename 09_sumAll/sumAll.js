0e822cad6e0173e21d882810f552534b1786ed22
Osama9798 <arifosama10@gmail.com>
2025-10-01 16:48:53 +0530

    All done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let min = 0;
+  let max = 0;
+  let sum = 0;
+  if (num1 < num2) {
+    min = num1;
+    max = num2;
+  } else {
+    min = num2;
+    max = num1;
+  }
+  while (min < max) {
+    sum += min;
+    min++;
+  }
+  return sum + max;
 };
 
 // Do not edit below this line
