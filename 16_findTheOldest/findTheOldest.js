442bc5d1173cdd4297f5090efbfa823e7e62fd82
Osama9798 <arifosama10@gmail.com>
2025-10-05 17:07:26 +0530

    All Done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.forEach((element) => {
+    if (!element.yearOfDeath) {
+      element.yearOfDeath = new Date().getFullYear();
+    }
+  });
 
+  const old = people
+    .map((element) => ({
+      age: element.yearOfDeath - element.yearOfBirth,
+      name: element.name,
+    }))
+    .sort((a, b) => b.age - a.age);
+  return old[0];
 };
 
 // Do not edit below this line
